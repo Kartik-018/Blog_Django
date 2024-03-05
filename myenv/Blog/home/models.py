e6fbe37270abe704f50b700308d01f04b0a2f80f
KARTIK DHANDHALA <kartikpaliwal991@gmail.com>
2024-03-05 11:32:23 +0530

    Blog model

--- myenv/Blog/home/models.py
+++ myenv/Blog/home/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from froala_editor.fields import FroalaField
+from .helpers import *
 # Create your models here.
+class BlogModel(models.Model):
+    tittle=models.CharField(max_length=1000)
+    content=FroalaField()
+    slug=models.SlugField(max_length=1000,null=True,blank=True)
+    image=models.ImageField(upload_to='blog')
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.tittle
+    
+    def save(self,*args,**kwargs):
+        self.slug=slugify(self.tittle)
+        super(BlogModel,self).save(*args,**kwargs)
+
+
+    
